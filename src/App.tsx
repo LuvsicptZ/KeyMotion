dffc50ad97897225eaaaf948299872c65f13cf6f
LuvsicptZ <Ryan197427@outlook.com>
2025-12-22 12:32:12 +1100

    feat: create initial app layout and container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Toaster } from "react-hot-toast";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex flex-col gap-4"> 
+      <div className="text-primary-400 font-medium">Choose time:</div>
 
-export default App
+      <Toaster />
+
+      <div className="relative text-3xl max-w-xl leading-relaxed break-all mt-3 align-justify">
+         <div className="text-slate-500">
+            Generating random words... 
+         </div>
+      </div>
+
+      <button className="mx-auto mt-10 text-slate-500">
+        Restart
+      </button>
+
+      <div className="mt-10">Results</div>
+    </div>
+  );
+};
+
+export default App;
