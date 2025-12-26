38754e780935b3eded5e8c0e091708e1b126e460
LuvsicptZ <Ryan197427@outlook.com>
2025-12-27 00:20:34 +1100

    refactor(useWords): extract generateWords helper

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -1,13 +1,16 @@
 import {faker} from "@faker-js/faker"
 import {useCallback, useState} from "react"
 
-const useWords = (count:number) => {
-    const [words, setWords] = useState<string>(() => {
-        return faker.word.words(count).toLowerCase();
-    });
+
+const generateWords = (count: number) => {
+    return faker.word.words(count).toLowerCase();
+}
+
+const useWords = (count: number) => {
+    const [words, setWords] = useState<string>(() => generateWords(count));
 
     const updateWords = useCallback(() => {
-        setWords(faker.word.words(count).toLowerCase());
+        setWords(generateWords(count));
     }, [count]);
 
     return {words, updateWords}
