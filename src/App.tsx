8a4e2815d57bf08eb184111a47a555eb0903214c
LuvsicptZ <Ryan197427@outlook.com>
2025-12-29 02:28:46 +1100

    refactor: integrate useEngine and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import './App.css'
 import RestartButton from "./components/RestartButton"
 import { Toaster } from "react-hot-toast";
+import useEngine from "./hooks/useEngine"
+import TimeSelecter from "./components/TimeSelecter"
+import GenerateWords from "./components/GenerateWords"
+import { useState } from "react"
+
 
 const WordsContainer = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,24 +15,37 @@ const WordsContainer = ({ children }: { children: React.ReactNode }) => {
 }
 
 const App = () => {
-  return (
-    <div className="flex flex-col gap-4"> 
-      <div className="text-primary-400 font-medium">Choose time:</div>
+  const { 
+    state, 
+    words, 
+    timeLeft, 
+    typed, 
+    errors, 
+    restart, 
+    totalTyped,
+    setCountdownSeconds 
+  } = useEngine()
 
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-3xl mx-auto px-4 mt-20">  
+
+      <TimeSelecter onTimeSelect={setCountdownSeconds} />
       <Toaster />
 
       <WordsContainer>
-        <div className="text-slate-500">
-          Generating random words... 
-        </div>
+        <GenerateWords words={words} />
       </WordsContainer>
 
       <RestartButton
-        className="mx-auto mt-10 text-slat-500"
+        className="mx-auto mt-10 text-slate-500"
         onRestart={() => restart()}
       />
 
-      <div className="mt-10">Results</div>
+      <div className="mt-10 flex gap-4 justify-center text-slate-500">
+        <div>Time: <span className="text-primary-400">{timeLeft}s</span></div>
+        <div>Errors: <span className="text-red-500">{errors}</span></div>
+        <div>Typed: <span className="text-green-500">{totalTyped.current}</span></div>
+      </div>
     </div>
   );
 };
