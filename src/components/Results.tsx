47c676a778075658c9699cad2cca9ee75a5a9cf1
LuvsicptZ <Ryan197427@outlook.com>
2025-12-31 13:55:00 +1100

    feat: enhance Results component with animations and styling

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -24,27 +24,28 @@ const Results = ({
         <motion.ul
         initial={initial}
         animate={animate}
-        className={`flex flex-col items-center ${className}`}
+        className={`flex flex-col items-center text-green-500 space-y-2 ${className}`}
         >
             <motion.li initial={initial} animate={animate} transition={{ duration: 0.3 }} className="text-xl">
                 <span className="text-2xl font-bold">Results</span>
             </motion.li>
 
-            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 0.3 }} className="text-xl">
+            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 0.3 }} className="">
                 Speed: {calculateWPM(total - errors, totalTime)} wpm
             </motion.li>
 
-            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 0.6 }} className="text-xl">
+            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 0.6 }} className="">
                 Accuracy: {accuracyPercentage.toFixed(2)}%
             </motion.li>
 
-            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 1.0 }} className="text-xl text-red-500">
+            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 1.0 }} className="">
+                Typed: {total}
+            </motion.li>
+
+            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 1.4 }} className="text-red-500">
                 Errors: {errors}
             </motion.li>
 
-            <motion.li initial={initial} animate={animate} transition={{ duration: 0.3, delay: 1.4 }} className="text-xl">
-                Typed: {total}
-            </motion.li>
         </motion.ul>
     )
 }
