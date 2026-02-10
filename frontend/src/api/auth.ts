849a46b1e99b63362eda6c0f14913af1a593bea5
LuvsicptZ <Ryan197427@outlook.com>
2026-02-10 13:21:46 +1100

    feat: add authentication API with user registration, login, and profile retrieval

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,43 @@
+import { input } from "framer-motion/client";
+import { request } from "./client";
+import { setToken } from "./token";
+
+export type User = {
+    id: string;
+    username: string;
+    email?: string;;
+    createdAt: string;
+};
+
+export type RegisterInput = {
+    username: string;
+    email: string;
+    password: string;
+  };
+  
+export type LoginInput = {
+    email: string;
+    password: string;
+};
+
+export type LoginResponse = {
+    accessToken: string;
+}
+
+export const register = async (input: RegisterInput) => 
+    request<User>("/auth/register", {
+        method: "POST",
+        body: JSON.stringify(input)
+    });
+
+export const login = async (input: LoginInput) => {
+    const data = await request<LoginResponse>("/auth/login", {
+        method: "POST",
+        body: JSON.stringify(input),
+    });
+    setToken(data.accessToken)
+    return data;
+}
+
+export const getMe = () => 
+    request<User>("/auth/me");
